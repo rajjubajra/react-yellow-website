71a96f4128ea6d4615a3fa81d967455467ac84e5
rajju <vajrachara@yahoo.com>
2019-02-25 13:23:31 +0000

    Updates

--- precache-manifest.a3b92bd7c8935dfeb8c03083cd5a0495.js
+++ precache-manifest.a3b92bd7c8935dfeb8c03083cd5a0495.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-yellow-website/static/js/runtime~main.a5bd91e7.js"
   },
   {
-    "revision": "caf5432614a388b78949",
-    "url": "/react-yellow-website/static/js/main.caf54326.chunk.js"
+    "revision": "cb0b77d1c37592e582e5",
+    "url": "/react-yellow-website/static/js/main.cb0b77d1.chunk.js"
   },
   {
     "revision": "6a089a0b58297bbf932a",
     "url": "/react-yellow-website/static/js/2.6a089a0b.chunk.js"
   },
   {
-    "revision": "caf5432614a388b78949",
-    "url": "/react-yellow-website/static/css/main.a0ecb492.chunk.css"
+    "revision": "cb0b77d1c37592e582e5",
+    "url": "/react-yellow-website/static/css/main.1c0d856c.chunk.css"
   },
   {
-    "revision": "0cb932fd54706b82d1e2d94b054f7fda",
+    "revision": "62f370e507a2912f931be36f66c433f2",
     "url": "/react-yellow-website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-yellow-website/precache-manifest.9a5b4948eef4e3afd24fc2fc6e0687ed.js"
+  "/react-yellow-website/precache-manifest.a3b92bd7c8935dfeb8c03083cd5a0495.js"
 );
 
 workbox.clientsClaim();
