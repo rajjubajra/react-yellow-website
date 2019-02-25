0b8be5027cd5251a5c3e734499ccc0973cc36c50
rajju <vajrachara@yahoo.com>
2019-02-25 06:02:35 +0000

    Updates

---
+++ precache-manifest.a8f97ae5f12a345826ed4782b39e0df3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5bd91e7cf4eb136fed6",
+    "url": "/react-yellow-website/static/js/runtime~main.a5bd91e7.js"
+  },
+  {
+    "revision": "166b06470bf34bc2b9a2",
+    "url": "/react-yellow-website/static/js/main.166b0647.chunk.js"
+  },
+  {
+    "revision": "652366c058029bf913d7",
+    "url": "/react-yellow-website/static/js/2.652366c0.chunk.js"
+  },
+  {
+    "revision": "166b06470bf34bc2b9a2",
+    "url": "/react-yellow-website/static/css/main.a0ecb492.chunk.css"
+  },
+  {
+    "revision": "cd61776d2909e3616034c3e6b66fd064",
+    "url": "/react-yellow-website/index.html"
+  }
+];
