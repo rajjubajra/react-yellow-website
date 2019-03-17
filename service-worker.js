01f9fa56d463852bd1285b876fccfc0f7fbf474c
rajju <vajrachara@yahoo.com>
2019-03-17 20:02:31 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-yellow-website/precache-manifest.c30ce74ac249d53875b36a195c12d9dd.js"
+  "/react-yellow-website/precache-manifest.2f47a8171717ad18c5cd038b0721178c.js"
 );
 
 workbox.clientsClaim();
